b69376c073e6b4f7e2ba828312fb0eba7037974a
Matthew Cole <colematthewg@gmail.com>
2019-06-17 21:22:37 -0500

    finished app push

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Books from "./pages/Books";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Detail from "./pages/Detail";
+import NoMatch from "./pages/NoMatch";
+import Nav from "./components/Nav";
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path="/" component={Books} />
+          <Route exact path="/books" component={Books} />
+          <Route exact path="/books/:id" component={Detail} />
+          <Route component={NoMatch} />
+        </Switch>
+        <Nav />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
